329570c0054b28e9f73fcbbbbf04b6394ec3329d
miaomiaoG <15827061459@163.com>
2020-05-09 10:29:06 +0800

    fabric4beta

--- daily-test/fabric4Beta12Test/poly.js
+++ daily-test/fabric4Beta12Test/poly.js
@@ -1,51 +1,39 @@
 const canvas = this.__canvas = new fabric.Canvas('c');
-// create a rect object
+
 const deleteIcon = 'data:image/svg+xml,%3C%3Fxml version=\'1.0\' encoding=\'utf-8\'%3F%3E%3C!DOCTYPE svg PUBLIC \'-//W3C//DTD SVG 1.1//EN\' \'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd\'%3E%3Csvg version=\'1.1\' id=\'Ebene_1\' xmlns=\'http://www.w3.org/2000/svg\' xmlns:xlink=\'http://www.w3.org/1999/xlink\' x=\'0px\' y=\'0px\' width=\'595.275px\' height=\'595.275px\' viewBox=\'200 215 230 470\' xml:space=\'preserve\'%3E%3Ccircle style=\'fill:%23F44336;\' cx=\'299.76\' cy=\'439.067\' r=\'218.516\'/%3E%3Cg%3E%3Crect x=\'267.162\' y=\'307.978\' transform=\'matrix(0.7071 -0.7071 0.7071 0.7071 -222.6202 340.6915)\' style=\'fill:white;\' width=\'65.545\' height=\'262.18\'/%3E%3Crect x=\'266.988\' y=\'308.153\' transform=\'matrix(0.7071 0.7071 -0.7071 0.7071 398.3889 -83.3116)\' style=\'fill:white;\' width=\'65.544\' height=\'262.179\'/%3E%3C/g%3E%3C/svg%3E';
 
 const img = document.createElement('img');
 img.src = deleteIcon;
 
-fabric.Object.prototype.transparentCorners = false;
-fabric.Object.prototype.cornerColor = 'blue';
-fabric.Object.prototype.cornerStyle = 'circle';
+const testPoints = [
+  {x: 10, y: 10},
+  {x: 50, y: 30},
+  {x: 40, y: 70},
+  {x: 60, y: 50},
+  {x: 100, y: 150},
+  {x: 40, y: 100},
+];
 
-function Add() {
-  var rect = new fabric.Rect({
-    left: 100,
-    top: 50,
-    fill: 'yellow',
-    width: 200,
-    height: 100,
-    objectCaching: false,
-    stroke: 'lightgreen',
-    strokeWidth: 4,
-  });
-
-  canvas.add(rect);
-  canvas.setActiveObject(rect);
-}
-
-fabric.Object.prototype.controls.llll = new fabric.Control({
-  position: {x: 0.5, y: -0.5},
-  offsetY: 16,
-  cursorStyle: 'pointer',
-  mouseUpHandler: deleteObject,
-  render: renderIcon,
-  cornerSize: 24,
+const poly = new fabric.Polygon([
+  {x: 10, y: 10},
+  {x: 50, y: 30},
+  {x: 40, y: 70},
+  {x: 60, y: 50},
+  {x: 100, y: 150},
+  {x: 40, y: 100},
+], {
+  stroke: 'red',
+  fill: 'rgba(0,0,0,0.02)',
+  left: 100,
+  top: 100,
+  hasControls: false,
+  hasBorders: false,
 });
 
-Add();
 
-function deleteObject(eventData, target) {
-  const canvas = target.canvas;
-  console.log(eventData, target);
-  // canvas.remove(target);
-  // canvas.requestRenderAll();
-}
-
-function renderIcon(ctx, left, top, styleOverride, fabricObject) {
-  console.log('renderIcon');
-  var size = this.cornerSize;
+function renderModifyPoint(ctx, left, top, styleOverride, fabricObject) {
+  console.log('draw point');
+  const size = this.cornerSize;
   ctx.save();
   ctx.translate(left, top);
   ctx.rotate(fabric.util.degreesToRadians(fabricObject.angle));
@@ -53,8 +41,27 @@ function renderIcon(ctx, left, top, styleOverride, fabricObject) {
   ctx.restore();
 }
 
+function handleMouseDown(eventData, target) {
 
-function renderModifyPoint(ctx, left, top, styleOverride, fabricObject) {
-  console.log('draw point');
+}
 
-};
+
+fabric.Object.prototype.controls.modifypoint = new fabric.Control({
+  position: {x: 10, y: 10},
+  offsetY: 16,
+  cursorStyle: 'crosshair',
+  render: renderModifyPoint,
+  cornerSize: 5,
+});
+/*
+ testPoints.forEach((point, index) => {
+ fabric.Object.prototype.controls[`modifypoint_${index}`] = new fabric.Control({
+ position: point,
+ cursorStyle: 'crosshair',
+ // mouseDownHandler: handleMouseDown,
+ render: renderModifyPoint,
+ cornerSize: 24,
+ });
+ });*/
+canvas.add(poly);
+canvas.renderAll();
